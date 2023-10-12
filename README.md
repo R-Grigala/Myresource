8a333bf540c622a4cb81df93417030228bc86fb9
R-Grigala <r.grigalashvili777@gmail.com>
2023-10-12 17:59:17 +0400

    Clear everything

--- README.md
+++
@@ -1,1 +0,0 @@
-# quizium-live
--- questions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('',questions , name="questions"),
-    path('api/get_quiz/' , get_quiz, name= "get_quiz"),
-    path('quiz/', quiz , name="quiz")
-]
