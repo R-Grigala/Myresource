ae6be6145a9f64a014c6316683a5935ee1c26891
R-Grigala <r.grigalashvili777@gmail.com>
2023-10-14 02:06:12 +0400

    Coding Grigala:// Add Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,10 +15,12 @@ import Layout from './hocs/Layout';
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+      </Routes>
       <Layout>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/reset-password" element={<ResetPassword />} />
           <Route exact path="/password/rest/confirm/:uid/:token" element={<ResetPasswordConfirm />} />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import logo from '../assets/brand/bootstrap-logo.svg';
 
 const Navbar = () => (
     <div>
-        Navbar
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid px-3">
+                <a className="navbar-brand" href="/home">
+                    <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top mx-2" />
+                        Bootstrap
+                </a>
+            </div>
+            
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav px-5">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <   a className="nav-link" href="/home">Features</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/home">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
 
 );
